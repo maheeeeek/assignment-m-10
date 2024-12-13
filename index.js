84a29f4f83dbbe2004a8fe29d3c83f31d9dfdbf1
JANNATUN NESA MAHEK <mahek6434@gmail.com>
2024-12-13 15:30:33 +0600

    Update index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+// 1
+
+function calculateDifference(){
+    let a = 10;
+    let b = 6;
+    let result = a-b;
+    console.log(result);
+}
+
+calculateDifference();
+
+// 2
+
+function Odd(){
+
+    let a = 5;
+
+    if(a % 2){
+        console.log(true);
+    }
+
+    else{
+        console.log(false);
+    }
+}
+
+Odd();
+
+// 3
+
+function findMin(){
+    
+}
